6fac79a7c36ec8413a0113bb867eab48a0fa6ebf
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-09-28 21:23:22 -0300

    tests: adding profile domain entity test

---
+++ internal/domain/entities/profile_test.go
@@ -0,0 +1,32 @@
+package entities
+
+import (
+	"testing"
+)
+
+func TestShouldDeclareProfileWithAllValidProperties(t *testing.T) {
+	name := "Akira"
+	var id int32 = 1
+	profile, err := NewProfile(id, name)
+	if profile == nil || err != nil {
+		t.Error("Should name and id be valid and create the profile")
+	}
+}
+
+func TestShouldReturnErrorWithInvalidName(t *testing.T) {
+	name := ""
+	var id int32 = 1
+	profile, err := NewProfile(id, name)
+	if profile != nil || err.Error() != "name cannot be empty" {
+		t.Error("Should return name is invalid")
+	}
+}
+
+func TestShouldReturnErrorWithInvalidId(t *testing.T) {
+	name := "akira"
+	var id int32 = 0
+	profile, err := NewProfile(id, name)
+	if profile != nil || err.Error() != "id cannot be 0" {
+		t.Error("Should return id is invalid")
+	}
+}
