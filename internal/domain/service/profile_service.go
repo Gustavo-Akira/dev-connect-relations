b8a8223266dbf174d023048929a4b316c232b32b
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-10-05 00:33:49 -0300

    feat: adding delete profile on profile service

--- internal/domain/service/profile_service.go
+++ internal/domain/service/profile_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"devconnectrelations/internal/domain/entities"
 	"devconnectrelations/internal/domain/ports/outbound/repository"
+	"errors"
 )
 
 type ProfileService struct {
@@ -16,4 +17,11 @@ func CreateNewProfileService(repo repository.ProfileRepository) *ProfileService 
 
 func (s *ProfileService) CreateProfile(ctx context.Context, profile *entities.Profile) (entities.Profile, error) {
 	return s.repository.CreateProfile(ctx, profile)
+}
+
+func (s *ProfileService) DeleteProfile(ctx context.Context, id int32) error {
+	if id == int32(0) {
+		return errors.New("id to delete cannot be 0")
+	}
+	return s.repository.DeleteProfile(ctx, id)
 }
--- internal/domain/service/profile_service_test.go
+++ internal/domain/service/profile_service_test.go
@@ -20,6 +20,12 @@ func (m *MockProfileRepository) CreateProfile(ctx context.Context, profile *enti
 	return args.Get(0).(entities.Profile), args.Error(1)
 }
 
+func (m *MockProfileRepository) DeleteProfile(ctx context.Context, id int32) error {
+	args := m.Called(ctx, id)
+
+	return args.Error(0)
+}
+
 func TestProfileService_CreateProfile(t *testing.T) {
 	ctx := context.Background()
 
@@ -67,5 +73,32 @@ func TestProfileService_CreateProfile(t *testing.T) {
 		assert.Empty(t, createdProfile)
 
 		mockRepo.AssertExpectations(t)
+	})
+}
+
+func TestProfileServic_DeleteProfile(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("should delete with success when profile is ok", func(t *testing.T) {
+		mockRepo := new(MockProfileRepository)
+		service := CreateNewProfileService(mockRepo)
+		mockRepo.On("DeleteProfile", ctx, int32(1)).Return(nil)
+		err := service.DeleteProfile(ctx, int32(1))
+		assert.Empty(t, err)
+	})
+
+	t.Run("should return error when profile return error", func(t *testing.T) {
+		mockRepo := new(MockProfileRepository)
+		service := CreateNewProfileService(mockRepo)
+		mockRepo.On("DeleteProfile", ctx, int32(1)).Return(errors.New("Some error"))
+		err := service.DeleteProfile(ctx, int32(1))
+		assert.Equal(t, err.Error(), "Some error")
+	})
+
+	t.Run("should return error when id is 0", func(t *testing.T) {
+		mockRepo := new(MockProfileRepository)
+		service := CreateNewProfileService(mockRepo)
+		err := service.DeleteProfile(ctx, int32(0))
+		assert.Equal(t, err.Error(), "id to delete cannot be 0")
 	})
 }
